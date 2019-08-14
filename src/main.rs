41123be1944798f354a8eeda14bcfe2a798f5d66
jdeepee <joshuadparkin@gmail.com>
2019-08-14 15:40:32 +0800

    Add core application setup for actix-web application using async on db and handlers

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,47 @@
-extern crate dotenv;
+#[macro_use] extern crate validator_derive;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate diesel;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use dotenv::dotenv;
+use std::io;
+use actix_web::{
+    web, App, HttpResponse, HttpServer, Error, middleware
+};
+use diesel::r2d2::ConnectionManager;
+use diesel::pg::PgConnection;
+use futures::future::{Future, ok};
 use std::env;
+use std::path::Path;
 
-fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello Jessy you have a cute mouth!")
+mod db;
+mod token;
+mod schema;
+
+fn register(data: web::Json<db::models::RegisterData>, pool: web::Data<db::Pool>) -> Box<dyn Future<Item = HttpResponse, Error = Error>> {
+    Box::new(ok::<_, Error>(
+        HttpResponse::Ok().content_type("text/html").body("Hello!"),
+    ))
 }
 
-fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
-}
+fn main() -> io::Result<()> {
+    let path = Path::new("./src/development.env");
+    dotenv::from_path(&path).expect("Unable to load .env");
+    let sys = actix_rt::System::new("api");
 
-fn main() {
-    dotenv().ok();
     let database_url = env::var("DATABASE_URL").unwrap_or_else(|e| {
         panic!("could not find {}: {}", "DATABASE_URL", e)
     });
-    HttpServer::new(|| {
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = db::Pool::new(manager).unwrap();
+
+    HttpServer::new(move || {
         App::new()
-            .route("/", web::get().to(index))
-            .route("/again", web::get().to(index2))
+            .data(pool.clone())
+            .wrap(middleware::Logger::default())
+            .route("/register", web::to_async(register))
     })
-    .bind("127.0.0.1:8088")
-    .unwrap()
-    .run()
-    .unwrap();
+    .bind("127.0.0.1:8080")?
+    .start();
+
+     println!("Started http server: 127.0.0.1:8080");
+    sys.run()
 }
