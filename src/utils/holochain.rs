26e87bcc5c42ae24a6d4e8ac2a1e9e0b392bfdc6
jdeepee <joshuadparkin@gmail.com>
2019-08-22 09:08:21 +0800

    Now mapping errors in assign_agent - can be used for logging if wanted

--- src/utils/holochain.rs
+++ src/utils/holochain.rs
@@ -1,19 +1,14 @@
 use std::fs;
-use actix_web::web;
-use diesel::result::Error;
 
-use crate::db::{
-    models,
-    Pool 
-};
+use crate::db::{models, Pool };
+use crate::errors;
 
 static KEY_DIR: &'static str = "/Users/Josh/.config/holochain/keys";
 
-pub fn assign_agent(pool: web::Data<Pool>) -> Result<String, Error> {
+pub fn assign_agent(pool: &Pool) -> Result<String, errors::JuntoApiError> {
     let possible_keys: Vec<_> = fs::read_dir(KEY_DIR).unwrap().map(|res| res.unwrap().path()).collect();
-    let n_users = models::Users::number_of_users(pool)?;
+    let n_users = models::Users::number_of_users(pool).map_err(|_err| errors::JuntoApiError::InternalError)?;
     println!("Number of users: {}", n_users);
     let split_path = possible_keys[n_users+1].to_str().unwrap().split("/").collect::<Vec<&str>>();
-    println!("Split path: {:?}", split_path);
     Ok(split_path[split_path.len()-1].to_string())
 }
