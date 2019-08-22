99bf1844307db504b2375bac02551f44db4bff9f
jdeepee <joshuadparkin@gmail.com>
2019-08-22 09:07:27 +0800

    Database operations fixed and error - using proper juntoapierror types which can be used by futures correctly

--- src/db/models/mod.rs
+++ src/db/models/mod.rs
@@ -1,12 +1,11 @@
-use actix_web::web;
+use actix_web::{error::Error as AWError};
 use validator::Validate;
 use uuid::Uuid;
-use diesel::result::Error;
-use diesel::pg::PgConnection;
 use diesel::prelude::*;
 
 use crate::schema::users;
-use crate::db::Pool;
+use crate::db::{Connection, Pool};
+use crate::errors;
 
 #[derive(Debug, Deserialize, Serialize, Validate)]
 pub struct RegisterData{
@@ -31,19 +30,31 @@ pub struct Users {
 }
 
 impl Users {
-    pub fn is_pub_address_in_use(comp_pub_key: String, pool: web::Data<Pool>) -> Result<bool, Error> {
+    pub fn is_pub_address_in_use(comp_pub_key: String, pool: &Pool) -> Result<bool, AWError> {
         use crate::schema::users::dsl::*;
 
-        let conn: &PgConnection = &pool.get().unwrap();
-        match users.filter(pub_address.eq(comp_pub_key)).first::<Users>(&*conn) {
+        let conn: Connection = pool.get().unwrap();
+        match users.filter(pub_address.eq(comp_pub_key)).first::<Users>(&conn) {
             Ok(_entry) => Ok(true),
             Err(_err) => Ok(false)
         }
     }
 
-    pub fn number_of_users(pool: web::Data<Pool>) -> Result<usize, Error> {
-        let conn: &PgConnection = &pool.get().unwrap();
-        let users_count = users::table.load::<Users>(&*conn)?;
+    pub fn number_of_users(pool: &Pool) -> Result<usize, errors::JuntoApiError> {
+        let conn: Connection = pool.get().unwrap();
+        let users_count = users::table.load::<Users>(&conn).map_err(|err| {
+            println!("Error: {}", err); //log err
+            errors::JuntoApiError::InternalError
+        })?;
         Ok(users_count.len())
+    }
+
+    pub fn insert_user(user: &Users, pool: &Pool) -> Result<(), AWError>{
+        let conn: Connection = pool.get().unwrap();
+        let _result: Users = diesel::insert_into(users::table)
+            .values(user)
+            .get_result(&conn)
+            .unwrap();
+        Ok(())
     }
 }
