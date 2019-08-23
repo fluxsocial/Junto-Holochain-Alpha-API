476763e72e1de580cae13a91eef94234ce659631
jdeepee <joshuadparkin@gmail.com>
2019-08-23 21:24:16 +0800

    Auth handler cleanups and remove un-used jwt imports

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,5 +1,6 @@
-use actix_web::{Error as AWError, error::ResponseError, HttpResponse, web};
+use actix_web::{error::ResponseError, HttpResponse, web};
 use actix_identity::Identity;
+use actix_threadpool::BlockingError;
 use futures::future::Future;
 use bcrypt::hash;
 
@@ -7,24 +8,26 @@ use crate::db;
 use crate::errors;
 use crate::utils;
 
-pub fn register(data: web::Json<db::models::RegisterData>, pool: web::Data<db::Pool>) -> impl Future<Item = HttpResponse, Error = AWError>  {
+pub fn register(
+    data: web::Json<db::models::RegisterData>, 
+    pool: web::Data<db::Pool>
+) -> impl Future<Item = HttpResponse, Error = errors::JuntoApiError> {
     web::block(move || {
         let pub_key = utils::holochain::assign_agent(&pool)?;
-        let id = uuid::Uuid::new_v4();
+        let user_id = uuid::Uuid::new_v4();
         let hashed_password = hash(data.password.clone(), 13).map_err(|_err| errors::JuntoApiError::InternalError)?;
-        let user = db::models::Users{id: id.clone(), email: data.email.clone(), password: hashed_password, pub_address: pub_key, 
+        let user = db::models::Users{id: user_id.clone(), email: data.email.clone(), password: hashed_password, pub_address: pub_key, 
                     first_name: data.first_name.clone(), last_name: data.last_name.clone()};
         db::models::Users::insert_user(&user, &pool).map_err(|_err| errors::JuntoApiError::InternalError)?;
-        let token = utils::jwt::Token::create(format!("{}", id)).map_err(|_err| errors::JuntoApiError::InternalError)?;
-        Ok(token)
+        Ok(user_id.to_string())
     })
-    .then(|token: Result<String, actix_threadpool::BlockingError<errors::JuntoApiError>>| match token {
-        Ok(token) => Ok(HttpResponse::Ok().json(json!({"token": token}))),
+    .then(|user_id: Result<String, BlockingError<errors::JuntoApiError>>| match user_id {
+        Ok(user_id) => Ok(HttpResponse::Ok().json(json!({"user_id": user_id}))),
         Err(err) => match err {
-                        actix_threadpool::BlockingError::Error(err) => {
+                        BlockingError::Error(err) => {
                             Ok(err.error_response())
                         },
-                        actix_threadpool::BlockingError::Canceled => {
+                        BlockingError::Canceled => {
                             Ok(HttpResponse::InternalServerError().into())
                         }
                     },
@@ -37,15 +40,15 @@ pub fn login(
     pool: web::Data<db::Pool>,
 ) -> impl Future<Item = HttpResponse, Error = errors::JuntoApiError> {
     web::block(move || db::models::Users::can_login(auth_data.into_inner(), &pool)).then(
-        move |res: Result<db::models::SlimUser, actix_threadpool::BlockingError<errors::JuntoApiError>>| match res {
+        move |res: Result<db::models::SlimUser, BlockingError<errors::JuntoApiError>>| match res {
             Ok(user) => {
                 let user_string = serde_json::to_string(&user).unwrap();
                 id.remember(user_string);
                 Ok(HttpResponse::Ok().finish())
             }
             Err(err) => match err {
-                actix_threadpool::BlockingError::Error(service_error) => Err(service_error),
-                actix_threadpool::BlockingError::Canceled => Err(errors::JuntoApiError::InternalError),
+                BlockingError::Error(service_error) => Err(service_error),
+                BlockingError::Canceled => Err(errors::JuntoApiError::InternalError),
             },
         },
     )
